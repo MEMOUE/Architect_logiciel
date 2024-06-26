92ac7a10a4a2c4eb2b8aabceddc44f63feb8eff8
MEMOUE <konememoue1998@gmail.com>
2024-06-26 23:43:29 +0000

    2em Commit

---
+++ actualite_polytechnicien/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'controlleur/ArticleController.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualité Polytechnicien</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<?php
+$controller = new ArticleController();
+$controller->displayArticles();
+?>
+</body>
+</html>
